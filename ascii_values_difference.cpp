f280ea5672e62319326619718a0f60a5a33339c8
Yuvansh Pratap Singh <83587170+Yuvansh-Pratap-Singh@users.noreply.github.com>
2023-03-25 22:56:46 +0530

    Create ascii_values_difference.cpp

---
+++ ascii_values_difference.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch1, ch2;
+    cout << "Enter the Characters : ";
+    cin >> ch1 >> ch2;
+    cout << "ASCII value difference is : " << ch1 - ch2;
+}
