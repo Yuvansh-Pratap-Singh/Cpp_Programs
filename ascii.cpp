cf41a41dfde1625d83ec186a1ce6bc9f9d44bfc2
Yuvansh Pratap Singh <83587170+Yuvansh-Pratap-Singh@users.noreply.github.com>
2023-03-26 11:47:47 +0530

    Create ascii.cpp

---
+++ ascii.cpp
@@ -0,0 +1,11 @@
+// ASCII, in full American Standard Code for Information Interchange, a standard data-encoding format for electronic communication between computers. ASCII assigns standard numeric values to letters, numerals, punctuation marks, and other characters used in computers.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout << "Enter Character :";
+    cin >> ch;
+    cout << "ASCII value is : " << int(ch);
+}
