6991626845ad398630e902a140aad29cf987722c
Yuvansh Pratap Singh <83587170+Yuvansh-Pratap-Singh@users.noreply.github.com>
2023-04-12 23:44:32 +0530

    Create fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter number : ";
+    cin >> n;
+    int a = 1, b = 1, sum = 0;
+    for (int i = 1; i <= n - 2; i++)
+    {
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+
+    cout << "Output : " << b << endl;
+}
