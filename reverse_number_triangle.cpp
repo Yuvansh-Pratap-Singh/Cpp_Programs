1c930172f3021ef5f39da0d752c83057647e6b00
Yuvansh Pratap Singh <83587170+Yuvansh-Pratap-Singh@users.noreply.github.com>
2023-05-02 00:49:17 +0530

    Reverse number triangle (Pattern)

---
+++ reverse_number_triangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter no. of rows : ";
+    cin >> a;
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 1; j <= a + 1 - i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
