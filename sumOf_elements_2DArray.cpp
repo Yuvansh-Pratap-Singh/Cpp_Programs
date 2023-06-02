c16aeec636ca8e0e0236c8939948fbedb9764e8b
Yuvansh Pratap Singh <83587170+Yuvansh-Pratap-Singh@users.noreply.github.com>
2023-06-02 23:08:49 +0530

    Sum of elements [2D Array]

---
+++ sumOf_elements_2DArray.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m, n;
+    cout << "Enter the no. of rows : ";
+    cin >> m;
+    cout << "Enter the no. of columns : ";
+    cin >> n;
+
+    int arr[m][n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+        cout << endl;
+    }
+
+    int sum = 0;
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            sum += arr[i][j];
+        }
+    }
+    cout << "Sum = " << sum;
+}
