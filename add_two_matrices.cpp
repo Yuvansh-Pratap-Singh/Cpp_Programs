dca0091f402913c7ebdaafdbbd7a8403b18f06ef
Yuvansh Pratap Singh <83587170+Yuvansh-Pratap-Singh@users.noreply.github.com>
2023-06-03 19:36:44 +0530

    Add two matrices [2D Array]

---
+++ add_two_matrices.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[2][3] = {1, 2, 3, 4, 5, 6};
+    int b[2][3] = {{1, 2, 3}, {4, 3, 2}};
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << a[i][j] + b[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
