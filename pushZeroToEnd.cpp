5bf4c7323f0bb0ed16a8e64ac196c03e8b742d36
Yuvansh Pratap Singh <83587170+Yuvansh-Pratap-Singh@users.noreply.github.com>
2023-06-30 21:29:03 +0530

    Push zero to end [Sorting]

---
+++ pushZeroToEnd.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {7, 0, 4, 0, 6, 2, 0, 3, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] == 0)
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
